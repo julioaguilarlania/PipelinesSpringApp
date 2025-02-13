7513f9b5dd5242dc7a42ba8f8244a07fba9a7bc3
Software QA LANIA <softwareqa@lania.edu.mx>
2025-02-13 17:10:02 -0600

    PMD: Modificador public innecesario

--- src/main/java/mx/lania/taller/repositorios/RepositorioVehiculos.java
+++ src/main/java/mx/lania/taller/repositorios/RepositorioVehiculos.java
@@ -11,14 +11,14 @@ public interface RepositorioVehiculos extends JpaRepository<Vehiculo,String> {
 
     @Query(value="SELECT * FROM vehiculos" +
             " WHERE kilometraje >= :k1 AND kilometraje <= :k2", nativeQuery = true)
-    public List<Vehiculo> buscarPorKilometraje(
+    List<Vehiculo> buscarPorKilometraje(
             @Param("k1") Double k1, @Param("k2") Double k2
     );
 
 
     @Query(value="SELECT placas,cliente_id,color,kilometraje FROM vehiculos" +
             " WHERE kilometraje >= :k1 AND kilometraje <= :k2", nativeQuery = true)
-    public List<Object[]> buscarPorKilometrajeDos(
+    List<Object[]> buscarPorKilometrajeDos(
             @Param("k1") Double k1, @Param("k2") Double k2
     );
 }
